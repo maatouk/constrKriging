70287eae611c5f11faba587876e0cf69a4b223b8
Hassan Maatouk <maatouk_hassan@hotmail.fr>
2024-12-03 21:25:38 +0100

    Update kmBounded.R

--- R/kmBounded.R
+++ R/kmBounded.R
@@ -20,10 +20,10 @@ kmBounded1D <- function(design, response,
                         upper = max(response)+(max(response)-min(response))*.1,
                         nugget = 1e-7*sd(response)) {
   
-  if (!is.matrix(design)) design=matrix(design, ncol=1)
+  if (!is.matrix(design)) design = matrix(design, ncol = 1)
   
   
-  #   if (coef.cov=="LOO") {
+  #   if (coef.cov == "LOO") {
   #     object = kmBounded1D(design, response, 
   #                                      basis.size = dim(design)[1]+2+10, 
   #                                      covtype,
@@ -33,9 +33,9 @@ kmBounded1D <- function(design, response,
   #                                      upper,
   #                                      nugget)
   #       
-  #       theta=coef.cov_LOO(object)
+  #       theta <- coef.cov_LOO(object)
   #     
-  #     model = NULL
+  #     model <- NULL
   #     while(is.null(model)) # auto raise nugget if needed
   #       try(model <- kmBounded1D(design, response, 
   #                                    basis.size, 
@@ -52,26 +52,26 @@ kmBounded1D <- function(design, response,
   n <- nrow(design) # number of design points
   N <- basis.size   # discretization size
   p <- (N + 1) - n  # degree of freedom
-  u <- seq(0, 1, by = 1/N) # discretization vector
+  u <- seq(from = 0, to = 1, by = 1/N) # discretization vector
   
   sig <- sqrt(coef.var)
   theta <- coef.cov
   
   
-  if(covtype=='gauss'){
+  if (covtype == 'gauss') {
     # Gaussian covariance kernel 
     k <- function(x, xp, sig, theta){
       sig^2*exp(-(x-xp)^2/(2*theta^2))
     }
-  }else if (covtype=='matern3_2'){
+  }else if (covtype == 'matern3_2') {
     # Matern 3/2 covariance kernel
-    k <- function(x, xp, sig, theta){
+    k <- function(x, xp, sig, theta) {
       sig^2*(1+(sqrt(3)*abs(x-xp)/theta))*exp(-sqrt(3)*abs(x-xp)/theta)
     }
   }
-  else if(covtype=="matern5_2"){
+  else if (covtype == "matern5_2") {
     # Matern 5/2 covariance kernel
-    k <- function(x, xp, sig, theta){
+    k <- function(x, xp, sig, theta) {
       sig^2*(1+sqrt(5)*(abs(x-xp))/theta+(5*(x-xp)^2)/(3*theta^2))*exp(-sqrt(5)*(abs(x-xp))/theta)
     }
   }
@@ -80,37 +80,37 @@ kmBounded1D <- function(design, response,
   
   
   # Basis functions  
-  phi <- function(x){
-    ifelse(x >= -1 & x <= 1, 1-abs(x), 0)
+  phi <- function(x) {
+    ifelse (x >= -1 & x <= 1, 1-abs(x), 0)
   }
-  phi0 <- function(x, N){
+  phi0 <- function(x, N) {
     phi(x*N)
   }
-  phiN <- function(x, N){
+  phiN <- function(x, N) {
     phi((x-u[N+1])*N)
   }
-  phii <- function(x, i, N){
+  phii <- function(x, i, N) {
     phi((x-u[i+1])*N)
   }
   
   A <- matrix(data = NA, ncol = N+1, nrow = n)
   A[, 1] = phi0(design[, 1], N)
   A[, N+1] = phiN(design[, 1], N)
-  for(j in 2 : N){
-    A[, j] = phii(design[, 1], j-1, N)
+  for (j in 2 : N) {
+    A[, j] <- phii(design[, 1], j-1, N)
   }
   
-  fctGamma=function(.theta){
+  fctGamma <- function(.theta) {
     Gamma <- matrix(data = NA, nrow = N+1, ncol = N+1)
-    for(i in 1 : (N+1)){
-      for(j in 1 : (N+1)){
+    for (i in 1 : (N+1)) {
+      for (j in 1 : (N+1)) {
         Gamma[i, j] = k(u[i], u[j], sig, .theta)
       }
     }
     Gamma <- Gamma + nugget * diag(N+1)
     return(Gamma)
   }
-  Gamma=fctGamma(theta)
+  Gamma <- fctGamma(theta)
   
   invGamma <- chol2inv(chol(Gamma))
   
@@ -118,14 +118,15 @@ kmBounded1D <- function(design, response,
   Amat1 <- rbind(A, Amat2)
   Amat <- rbind(Amat1, -Amat2)
   
-  zetoil <- solve.QP(invGamma, dvec=rep(0, N+1), Amat=t(Amat), bvec=c(response, rep(lower, N+1), rep(-upper, N+1)), meq=n)$solution
+  zetoil <- solve.QP(Dmat = invGamma, dvec = rep(0, N+1), Amat = t(Amat), 
+                     bvec = c(response, rep(lower, N+1), rep(-upper, N+1)), meq = n)$solution
   
   return(structure(
-    list(zetoil=zetoil, phi0=phi0, phiN=phiN, phii=phii, Amat=Amat, Gamma=Gamma, A=A, D=D, 
-         fctGamma=fctGamma, invGamma=invGamma,
-         call=list(design=design, response=response, basis.size=basis.size, covtype=covtype,
-                   coef.cov=coef.cov, coef.var=coef.var, nugget=nugget, lower=lower,
-                   upper=upper)), class="kmBounded1D"))
+    list(zetoil = zetoil, phi0 = phi0, phiN = phiN, phii = phii, Amat = Amat, Gamma = Gamma, A = A, D = D, 
+         fctGamma = fctGamma, invGamma = invGamma,
+         call = list(design = design, response = response, basis.size = basis.size, covtype = covtype,
+                   coef.cov = coef.cov, coef.var = coef.var, nugget = nugget, lower = lower,
+                   upper = upper)), class = "kmBounded1D"))
 }
 
 
@@ -135,15 +136,15 @@ kmBounded1D <- function(design, response,
 #' @param model km* model
 #' @param newdata data in design space
 #' @method Phi1D kmBounded
-Phi1D.kmBounded1D <- function(model, newdata){
+Phi1D.kmBounded1D <- function(model, newdata) {
   N <- model$call$basis.size  
   x <- newdata
   
-  v <- matrix(NA, nrow = length(x), ncol = N+1)
+  v <- matrix(data = NA, nrow = length(x), ncol = N+1)
   v[, 1] <- model$phi0(x, N = N)
   v[, N+1] <- model$phiN(x, N = N)
-  for(j in 2 : N){
-    v[, j] = model$phii(x, j-1, N = N)
+  for (j in 2 : N) {
+    v[, j] <- model$phii(x, j-1, N = N)
   }
   
   return(v)
